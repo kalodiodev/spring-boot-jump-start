aff3ef0e584c46fda8f9411a0df2dda8df72af08
Athanasios Raptodimos <thanosra@gmail.com>
2017-07-22 18:09:56 +0300

    Add Index Controller Test

--- src/main/java/eu/kalodiodev/springjumpstart/controller/IndexController.java
+++ src/main/java/eu/kalodiodev/springjumpstart/controller/IndexController.java
@@ -3,6 +3,11 @@ package eu.kalodiodev.springjumpstart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Index Controller
+ * 
+ * @author Athanasios Raptodimos
+ */
 @Controller
 public class IndexController {
 	
