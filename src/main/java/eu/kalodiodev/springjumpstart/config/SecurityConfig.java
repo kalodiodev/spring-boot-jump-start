e9bba29ee2391b34486d3c3cb8187a46248b7a45
Athanasios Raptodimos <thanosra@gmail.com>
2017-07-23 14:01:04 +0300

    Add password encoder bean

--- src/main/java/eu/kalodiodev/springjumpstart/config/SecurityConfig.java
+++ src/main/java/eu/kalodiodev/springjumpstart/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package eu.kalodiodev.springjumpstart.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Security Configuration
@@ -15,6 +18,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableGlobalMethodSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	/**
+     * Password Encoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
 	
 	/* Permit all matchers */
 	private static final String[] PUBLIC_MATCHERS = {
