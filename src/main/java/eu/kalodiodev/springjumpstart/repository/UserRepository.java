eba2c7f64220533a26319704c9db50a67db2eaae
Athanasios Raptodimos <thanosra@gmail.com>
2017-08-01 15:49:47 +0300

    Add password update to user repository

--- src/main/java/eu/kalodiodev/springjumpstart/repository/UserRepository.java
+++ src/main/java/eu/kalodiodev/springjumpstart/repository/UserRepository.java
@@ -1,6 +1,11 @@
 package eu.kalodiodev.springjumpstart.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import eu.kalodiodev.springjumpstart.domain.User;
 
@@ -9,6 +14,7 @@ import eu.kalodiodev.springjumpstart.domain.User;
  * 
  * @author Athanasios Raptodimos
  */
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 	
 	/**
@@ -18,4 +24,15 @@ public interface UserRepository extends CrudRepository<User, Long> {
 	 * @return user
 	 */
     User findByEmail(String email);
+    
+    /**
+     * Update user's password
+     * 
+     * @param userId Id of user
+     * @param password New password
+     */
+    @Transactional
+    @Modifying
+    @Query("update User u set u.encryptedPassword = :password where u.id = :userId")
+    void updateUserPassword(@Param("userId") Long userId, @Param("password") String password);
 }
--- src/test/java/eu/kalodiodev/springjumpstart/repository/UserRepositoryTest.java
+++ src/test/java/eu/kalodiodev/springjumpstart/repository/UserRepositoryTest.java
@@ -3,11 +3,7 @@ package eu.kalodiodev.springjumpstart.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import eu.kalodiodev.springjumpstart.domain.User;
 
@@ -16,33 +12,36 @@ import eu.kalodiodev.springjumpstart.domain.User;
  * 
  * @author Athanasios Raptodimos
  */
-@RunWith(SpringRunner.class)
-@DataJpaTest
-public class UserRepositoryTest {
-	
-	@Autowired
-    private TestEntityManager entityManager;
-	
+public class UserRepositoryTest extends AbstractRepositoryTest {
+		
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Test
 	public void whenFindByEmail_thenReturnUser() {
 	    // given
-	    User user = new User();
-	    user.setFirstName("John");
-	    user.setLastName("Doe");
-	    user.setEmail("johndoe@examle.com");
-	    user.setPassword("12345678");
-	    user.setEncryptedPassword("dfsdfsdfsdf");
-	   
-	    entityManager.persist(user);
-	    entityManager.flush();
+	    User user = persistTestUser();
 	 
 	    // when
 	    User found = userRepository.findByEmail(user.getEmail());
 	 
 	    // then
 	    assertThat(found.getEmail()).isEqualTo(user.getEmail());
+	}
+	
+	@Test
+	public void shouldUpdateUserPassword() {
+		// given
+	    User user = persistTestUser();
+	    	    	    
+	    String newPassword = "0000000";
+	    
+	    // when
+	    userRepository.updateUserPassword(user.getId(), newPassword);
+	    	        
+	    // then
+	    entityManager.clear();
+	    User updatedUser = entityManager.find(User.class, user.getId());
+	    assertThat(updatedUser.getEncryptedPassword()).isEqualTo(newPassword);
 	}
 }
